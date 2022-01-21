138e6f6bb4af91946f32b49467156165f267ee9c
Mei <74983120+meixsu@users.noreply.github.com>
2022-01-21 23:50:51 +0800

    update: add date prop

--- components/CommentInfo.vue
+++ components/CommentInfo.vue
@@ -1,11 +1,17 @@
 <script>
 export default {
-  props: ["image", "username"],
+  props: ["image", "username", "createdAt"],
 };
+</script>
+
+<script setup>
+import { useTimeAgo } from "@vueuse/core";
 </script>
 
 <template>
   <div class="flex items-center gap-x-3">
     <img :src="image" :alt="username" class="h-8" />
+    <p>{{ username }}</p>
+    <p>{{ useTimeAgo(new Date(createdAt)) }}</p>
   </div>
 </template>
