84b72c868986bdb248a28e8572d9eea95e9afed1
Mei <74983120+meixsu@users.noreply.github.com>
2022-01-21 23:25:44 +0800

    update: fixed layout

--- components/AddComment.vue
+++ components/AddComment.vue
@@ -6,7 +6,14 @@ export default {
 
 <template>
   <div class="bg-white px-5 py-4">
-    <textarea class="w-full" placeholder="Add a comment..."></textarea>
-    <img :src="currentUser.image.webp" :alt="currentUser.username" />
+    <div class="flex items-center flex-wrap gap-y-4">
+      <textarea class="basis-full" placeholder="Add a comment..."></textarea>
+      <img
+        :src="currentUser.image.webp"
+        :alt="currentUser.username"
+        class="h-8"
+      />
+      <button class="btn-primary ml-auto">SEND</button>
+    </div>
   </div>
 </template>
