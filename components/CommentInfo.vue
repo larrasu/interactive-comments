940fd765e9b9e052b662fc3cb7355de8f811cb9a
Mei <74983120+meixsu@users.noreply.github.com>
2022-01-21 23:35:14 +0800

    add: comment image and username

---
+++ components/CommentInfo.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+  props: ["image", "username"],
+};
+</script>
+
+<template>
+  <div class="flex items-center gap-x-3">
+    <img :src="image" :alt="username" class="h-8" />
+  </div>
+</template>
