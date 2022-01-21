c54208c9fce2d7b9ee9c48924381dd363628a609
Mei <74983120+meixsu@users.noreply.github.com>
2022-01-21 23:50:32 +0800

    update: add date prop

--- components/Comment.vue
+++ components/Comment.vue
@@ -1,3 +1,15 @@
+<script setup>
+defineProps({
+  comment: Object,
+});
+</script>
+
 <template>
-  <div class="bg-white"></div>
+  <div class="bg-white px-5 py-4">
+    <CommentInfo
+      :image="comment.user.image.webp"
+      :username="comment.user.username"
+      :createdAt="comment.createdAt"
+    />
+  </div>
 </template>
